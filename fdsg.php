9939ab89d61fcb8d0e8287e25df97bd209dc5d11
SergeyRahmanin <rahmaninsv@mail.ru>
2021-09-13 12:39:56 +0400

    Пе6рвый

---
+++ fdsg.php
@@ -0,0 +1,37 @@
+<?php
+class Car
+{
+    public $color;
+    public $year;
+    public $manufacturer;
+
+    public function __construct($color, $year, $izgotovitel)
+    {
+        $this->color = $color;
+        $this->year = $year;
+        $this->manufacturer = $izgotovitel;
+    }
+    public function startEngine()
+    {
+        /**
+        большой кусок кода завода мотора
+         */
+    }
+
+    public function go()
+    {
+        /**
+        реализация езда
+         */
+    }
+}
+
+$myCar = new Car('red',2017,'Mersedes'); //новый слепок
+
+
+
+$myCar->startEngine();
+$myCar->go();
+
+$myCar2 = new Car();
+$myCar3 = new Car();<?php
